86e701dd0a9f5b49415b062a7b539aecf7594cd2
CN0105 <cn0105@CN0105.lan>
2019-06-19 17:06:30 +0100

    Updates

---
+++ precache-manifest.1c0d2f9bc73d5d26331bb778ecf778d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9105d99d20380bd3be34c749d0fce46d",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "4447988e665e0ee03144",
+    "url": "/reactcalculator/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/reactcalculator/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "4447988e665e0ee03144",
+    "url": "/reactcalculator/static/js/main.1553163f.chunk.js"
+  },
+  {
+    "revision": "72f3015dd0be49f3d573",
+    "url": "/reactcalculator/static/js/runtime~main.57313a7a.js"
+  }
+]);
